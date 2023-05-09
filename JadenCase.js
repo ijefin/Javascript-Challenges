a04e8686844b8fe9fdc81bd43c7cb30790e1b9bb
ijefiN <jeffersonmecmiler@hotmail.com>
2023-05-09 16:36:20 -0300

    finished counChar challenge

---
+++ JadenCase.js
@@ -0,0 +1,45 @@
+/**
+ * This function return all words capitalized on their first position.
+ * @param {string} text - the text/phrase.
+ * @returns {string} All words capitalized.
+ */
+const jadenCase = (phrase) => {
+  const words = phrase.split(" ");
+  const capitalized = words.map((l) => l[0].toUpperCase() + l.slice(1));
+  return console.log(capitalized.join(" ")); //remove console log
+};
+
+const text = "O rato roeu a roupa do rei de roma";
+
+// jadenCase(text);
+
+/**
+ * This function return all words larger than five reversed.
+ * @param {string} text - the text/phrase.
+ * @returns {string} All words capitalized.
+ */
+const spinWords = (string) => {
+  const words = string.split(" ");
+  const reverseLargerThan4 = words.map((i) =>
+    i.length > 4 ? i.split("").reverse().join("") : i
+  );
+  return console.log(reverseLargerThan4.join(" ")); //remove console log
+};
+
+// spinWords("pare de inverter minhas letras caralho");
+
+const countCharacteres = (string) => {
+  const result = {};
+
+  for (let index = 0; index < string.length; index++) {
+    const char = string[index];
+
+    result[char] ? result[char]++ : (result[char] = 1);
+  }
+  return console.log(result);
+};
+
+// countCharacteres("otorrinolaringologista");
+
+const obj = {}
+
